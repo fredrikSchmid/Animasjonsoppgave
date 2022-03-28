fb937b4685479b74da7eeafddcd5cda10ea3c119
fredrikSchmid <fredd.fsb@gmail.com>
2022-03-28 15:42:52 +0200

    Elling har lite gull

---
+++ app.js
@@ -0,0 +1,11 @@
+
+
+
+window.onload = function() {
+    
+}
+
+var c = document.getElementById("mittCanvas");
+var ctx = c.getContext("2d");
+var img = document.getElementById("spaceShip");
+ctx.drawImage(img, 100, 100);
