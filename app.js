21bfe2ce6f0165634ca2d05cc23b2aa30c7f7c45
fredrikSchmid <fredd.fsb@gmail.com>
2022-05-09 16:13:26 +0200

    Ferdig kommentert kode

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
+
+// Forskjellige variabler som blir brukt i spillet
 var poengsum = 1;
 var nivå = 1;
-
 var ødelagtAlien = 0;
 var poengEl = document.getElementById("poeng");
 var nivåEl = document.getElementById("nivå");
@@ -15,17 +16,27 @@ var ctx = c.getContext("2d");
 var animasjonID;
 var restartEl = document.getElementById("restart");
 var gameOver = false;
+var spaceShipImg = document.getElementById("spaceShip");
+var laserbeamImg = document.getElementById("laser");
+var skudd = [];
+var fiende = [];
+var sisteSkudd = 0;
+var z = 1;
 
-restartEl.addEventListener("click", restartButton);
+
+//Eventlisteners som sjekker om og hvilke taster som blir trykket på og Restart-knappen
 window.addEventListener("keydown", mottaTaster);
 window.addEventListener("keyup", mottaTaster);
+restartEl.addEventListener("click", restartButton);
 
+
+//Restart-knappens funksjon som laster inn nettsiden på nytt og hindrer at den blir trykket på flere ganger på rad
 function restartButton() {
   window.location.reload();
   restartEl.blur();
 }
 
-
+//Sjekker hvilke knapper som blir trykket på og sjekker også om "R" blir trykket på, isåfall restartes siden
 function mottaTaster(e) {
   if (e.type == "keydown") {
     knapper[e.keyCode] = true;
@@ -39,10 +50,7 @@ function mottaTaster(e) {
   
 }
 
-
-
-
-//Kjører poengteller funskjonen  som legger til 2 poeng hvert sek.
+//Kjører poengteller funskjonen  som legger til poeng og forandrer vGrad ettersom hva som er valgt
  setInterval(
  function poengteller() {
    if (nivå < 101 && gameOver == false) {
@@ -50,42 +58,31 @@ function mottaTaster(e) {
     
      if (poengsum % 10 == 0) {
        nivåEl.innerHTML = "Nivå:" + nivå;
-
        nivå++;
      }
 
      if (lettEl.checked == true) {
-     vGrad = lettEl.value;
-     lettEl.blur();
-     x = 1000;
-
+        vGrad = lettEl.value;
+        lettEl.blur();
      } else if (midEl.checked == true) {
-     vGrad = midEl.value;
-     midEl.blur();
-     x = 500;
+        vGrad = midEl.value;
+        midEl.blur();
      } else if (hardEl.checked == true) {
-     vGrad = hardEl.value;
-     hardEl.blur();
-     x = 250;
+        vGrad = hardEl.value;
+        hardEl.blur();
      }
-     
      poengsum++;
    }
-}, x);
-
-
+}, 500);
 
 //Gjør canvasen responsiv til skjermer med høyere/lavere oppløsning
-
 window.onload = function() {
      c.width = window.innerWidth * window.devicePixelRatio * 0.6;
      c.height = window.innerHeight * window.devicePixelRatio * 0.75;
 
 }
 
-
-
-
+//Klassen til Spilleren
 class Spiller {
   constructor(x, y, fart) {
     this.x = x;
@@ -93,8 +90,8 @@ class Spiller {
     this.fart = fart;
   }
 
+  //Funksjonen som gir spilleren fart når piltastene trykkes inn
   flytt() {
-       //Gir spilleren fart når piltastene trykkes inn
     if (knapper[37]) {
       spiller.x -= spiller.fart;
     }
@@ -108,14 +105,14 @@ class Spiller {
       spiller.y += spiller.fart;
     }
 
-    //Hindrer spilleren i å bevege seg ut av canvas
-    if (this.x - 10 < 0) {
+    //Hindrer spilleren i å bevege seg ut av canvaset
+    if (this.x < 0) {
       spiller.x += this.fart;
     }
     if (this.x + 80 > c.width) {
       spiller.x -= this.fart;
     }
-    if (this.y  < 0) {
+    if (this.y < 0) {
       spiller.y += this.fart;
     }
     if (this.y + 80 > c.height) {
@@ -123,12 +120,13 @@ class Spiller {
     }
   }
 
+  //Funksjonen som tegner opp skipet
   tegn() {
-    var spaceShipImg = document.getElementById("spaceShip");
     ctx.drawImage(spaceShipImg, this.x, this.y, 80, 80);
   }
 }
 
+//Klassen til fienden/fiendene
 class Fiende {
   constructor(x, y, fart) {
     this.x = x;
@@ -140,24 +138,20 @@ class Fiende {
     this.bilde = this.alienbilde;
   }
 
+  //Funksjonen som flytter på de, farten endres utifra vGrad
   flytt() {
-    this.x += this.xFart;
-
     if (vGrad == "lett") {
-      this.y += this.yFart / 4;
-      this.x += this.xFart / 4;
-
+      this.y += this.yFart / 1.5;
+      this.x += this.xFart / 1.5;
     } else if (vGrad == "mid") {
       this.y += this.yFart;
       this.x += this.xFart;
     } else {
       this.y += this.yFart * 3;
-      this.x += this.xFart;
-
-
-
+      this.x += this.xFart * 3;
     }
 
+    //Hindrer fiendene i å bevege seg ut av canvaset
     if (this.x - 10 < 0 || this.x + 90 > c.width) {
       this.xFart = -this.xFart;
     }
@@ -167,21 +161,25 @@ class Fiende {
     }
   }
 
+  //Tegner opp fienden
   tegn() {
     ctx.drawImage(this.bilde, this.x, this.y, 80, 55);
   }
 
+  //Gjør bildet om til eksplosjon og lagrer tidspunktet
   ødelagt() {
     this.tidSkutt = Date.now();
     this.bilde = this.expobilde;
   }
 
+  //Stopper fienden
   skutt() {
     this.yFart = 0;
     this.xFart = 0;
   }
 }
 
+//Klasseb til skuddet
 class Skudd {
   constructor(x, y, fart) {
     this.x = x;
@@ -190,35 +188,25 @@ class Skudd {
     this.xFart = fart;
   }
 
+  //Tegner opp skuddet
   tegn() {
-   var laserbeamImg = document.getElementById("laser");
     ctx.drawImage(laserbeamImg, this.x, this.y, 19.1, 50)
   }
 
+  //Flytter på skuddet
   flytt() {
        this.y -= this.yFart;
   }
 
 }
 
-
+//Lager en ny spiller med klassen
 var spiller = new Spiller(c.width/2.5, c.height/1.69, 6);
 
-var fiende = [];
-
-
+//Lager en ny fiende med klassen og dytter den inn i arrayen
 fiende.push(new Fiende(c.width/4, c.height/4, Math.random()*3+2));
 
-var skudd = [];
-
-var sisteSkudd = 0;
-
-var z = 1;
-
-// var hinder2 = new Hinder(canvas.width/5, canvas.height/4, "yellow", 5);
-
-
-
+//Funksjon som finner avstanden mellom to ting
 function finnAvstand(obj1, obj2) {
 
   var xAvstand2 = Math.pow(obj1.x - obj2.x, 2);
@@ -229,105 +217,94 @@ function finnAvstand(obj1, obj2) {
   return avstand;
 }
 
-
-
- 
-
-
+//Animasjonen
 function animer() {
   ctx.clearRect(0, 0, c.width, c.height);
 
-    for( var i = 0; i < fiende.length; i++) {
+  //Tegner opp og flytter alle fiender i array
+  for( var i = 0; i < fiende.length; i++) {
     fiende[i].flytt();
     fiende[i].tegn();
+  }
 
-    }
-
-
+  //Tegner opp og flytter spilleren
   spiller.flytt();
   spiller.tegn();
 
-  
-
+  //Lager skudd med plassering på hver side av spillerens skip hvis det har gått 500ms og dytter de inn i array
   if (knapper[32] && Date.now() - sisteSkudd > 500) {
     if (z == 1) {
       skudd.push(new Skudd(spiller.x + 14, spiller.y - 20, 7));
       z = 2;
     } else if (z == 2) {
       skudd.push(new Skudd(spiller.x + 60, spiller.y - 20, 7));
-
       z = 1;
     }
-
     sisteSkudd = Date.now();
   }
 
+  //Tegner opp alle skuddene i array
   for (i = 0; i < skudd.length; i++) {
     skudd[i].flytt();
     skudd[i].tegn();
   }
 
-
-   for (var i = 0; i < skudd.length; i++) {
+  //Fjerner skudd som er over canvaset
+  for (var i = 0; i < skudd.length; i++) {
     if (skudd[i].y < 0) {
       skudd.splice(i, 1);
-
       break
     }
-     for (var j = 0; j < fiende.length; j++) {
-       if (finnAvstand(skudd[i], fiende[j]) < 50) {
-         skudd.splice(i, 1);
 
-         fiende[j].ødelagt();
-         fiende[j].skutt();
+    //Sjekker om skuddene treffer fienden og kjører utifra det
+    for (var j = 0; j < fiende.length; j++) {
+      if (finnAvstand(skudd[i], fiende[j]) < 50) {
+        skudd.splice(i, 1);
 
-         ødelagtAlien = 1;
+        fiende[j].ødelagt();
+        fiende[j].skutt();
 
-         if (vGrad == "lett") {
+        ødelagtAlien = 1;
+
+        //Gir ekstra poeng når du treffer utifra vGrad
+        if (vGrad == "lett") {
           poengsum = poengsum + 2;
         } else if (vGrad == "mid") {
-          poengsum = poengsum + 5;
+            poengsum = poengsum + 5;
         } else if (vGrad == "hard") {
-          poengsum = poengsum + 10;
+            poengsum = poengsum + 10;
         }
- 
-         break
-       }
-     }
-   }
-
-var nyTid = Date.now();
-
-  for(var i = 0; i < fiende.length; i++) {
-          if(ødelagtAlien == 1){
-
-            
-
-            if(nyTid - fiende[i].tidSkutt  > 1000) {
-
-              fiende.splice(i, 1);
-
-              ødelagtAlien = 0;
-
-              if(vGrad == "lett") {
-              fiende.push(new Fiende(Math.random()*c.width-50, Math.random()*c.height - 20, 2));
-              } else if (vGrad == "mid") {
-                fiende.push(new Fiende(Math.random()*c.width-50, Math.random()*c.height - 200, 2));
-                fiende.push(new Fiende(Math.random()*c.width-50, Math.random()*c.height - 20, 2));
-              } else {
-                fiende.push(new Fiende(Math.random()*c.width-50, Math.random()*c.height - 20, 2));
-                fiende.push(new Fiende(Math.random()*c.width-50, Math.random()*c.height - 20, 2));
-                fiende.push(new Fiende(Math.random()*c.width-50, Math.random()*c.height - 20, 2));
-              }
-              
-
-
-
-
+        break
       }
     }
   }
 
+  //Lagrer tiden akkurat nå for å bruke i neste funksjon
+  var nyTid = Date.now();
+
+  //Går gjennom alle fiendene i arrayen og sjekker om de er ødelagt, lar de være ødelagt i 1000ms før de fjernes og det
+  //legges til nye fiender. Antall nye fiender utifra vGrad.
+  for(var i = 0; i < fiende.length; i++) {
+    if(ødelagtAlien == 1){
+      if(nyTid - fiende[i].tidSkutt  > 1000) {
+        fiende.splice(i, 1);
+        ødelagtAlien = 0;
+
+        if(vGrad == "lett") {
+          fiende.push(new Fiende(Math.random()*(c.width/1.2), Math.random()*(c.height/2), 2));
+        } else if (vGrad == "mid") {
+            fiende.push(new Fiende(Math.random()*(c.width/1.2), Math.random()*(c.height/2), 2));
+            fiende.push(new Fiende(Math.random()*(c.width/1.2), Math.random()*(c.height/2), 2));
+        } else {
+            fiende.push(new Fiende(Math.random()*c.width-50, Math.random()*(c.height/2), 2));
+            fiende.push(new Fiende(Math.random()*c.width-50, Math.random()*(c.height/2), 2));
+            fiende.push(new Fiende(Math.random()*c.width-50, Math.random()*(c.height/2), 2));
+        }
+      }
+    }
+  }
+
+  //Sjekker om spiller og en fiende er på hverandre, skriver på canvaset og setter gameOver = true
   for(var i = 0; i < fiende.length; i++) {
     if (finnAvstand(fiende[i], spiller) < 50) {
       ctx.font = "30px Courier New";
@@ -339,22 +316,18 @@ var nyTid = Date.now();
       ctx.font = "17px Courier New"
       ctx.fillText("Trykk på 'R' eller Restart-knappen for å starte på nytt", c.width/2, c.height/1.4)
       gameOver = true;
-
-      }
-         
+    }
   }
 
+  //Stopper spillet hvis gameOver = true, ellers fortsetter spillet av seg selv
   if (gameOver == false) {
-  animasjonID = requestAnimationFrame(animer); 
+    animasjonID = requestAnimationFrame(animer); 
   } else if (gameOver == true){
     cancelAnimationFrame(animasjonID);
-
   }
-
-  
-
 }
 
+//Kjører animasjonen en gang for å sette den i gang
 animasjonID = requestAnimationFrame(animer);
 
 
